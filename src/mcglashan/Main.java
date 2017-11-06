60be21c5a27d1283bd7db7caaae76ca1094a16ca
Scott-McG <scott.mcglashan@ocsbstudent.ca>
2017-11-06 13:35:07 -0500

    new save

---
+++ src/mcglashan/Main.java
@@ -0,0 +1,13 @@
+package mcglashan;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World Again");
+		System.out.println("5"+"34");
+		System.out.println("\tHey\n How you doing");
+	}
+
+}
